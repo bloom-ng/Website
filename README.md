c010a21fbeda9b9e2224300ba64e90ce5a47f6fa
bloom-ng <it@bloomdigitmedia.com>
2022-06-29 01:08:32 +0100

    Initialized Repo
    Created The Project Template

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Website
-Companies Frontend
+3. Use the DevNotes.txt to mention where Custom css where use i.e Important
+4. Avoid inline styling
+5. Try as much as possible to avoid custom styling
+
+****Page List Done****
+======================
+
+- .[_]. Homepage
+- .[_]. About page
+- .[_]. Blog page
+- .[_]. Service page
+- .[_]. Our works
+- .[_]. FAQ page
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bloom-black'  : '#1D1D1D',
+        'bloom-orange' : '#FF8100',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      }
+    },
+    
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
